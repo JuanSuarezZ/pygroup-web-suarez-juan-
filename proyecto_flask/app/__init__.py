225c81c882f632833acf0b42a24a498279c66ee6
JuanSuarezZ <jussuarezc@correo.udistrital.edu.co>
2020-11-05 21:01:31 -0500

    tarea

--- proyecto_flask/app/__init__.py
+++ proyecto_flask/app/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 
@@ -6,11 +8,9 @@ app = Flask(__name__)
 #from app.products.views import products
 
 
-
-@app.route('/mirutaa')
-def index():
-    return "hola pygroup"
-
+@app.route('/mirutaa/<name>')
+def hindex(name=None):
+    return render_template('hello.html', name=name)
 
 
 if __name__ == "__main__":
